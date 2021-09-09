635118b2c1294230db2faf1feb7017a1b71571d1
Osanda Deshan <osanda.deshan@gmail.com>
2021-09-09 09:55:52 +0530

    Added reporting and TestNG | Code refactoring

--- src/main/page/GooglePage.java
+++ src/main/page/GooglePage.java
@@ -3,6 +3,15 @@ package page;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
 
+/**
+ * Project Name    : selenide-page-object-demo
+ * Developer       : Osanda Deshan
+ * Version         : 1.0.0
+ * Date            : 9/9/2021
+ * Time            : 8:47 AM
+ * Description     : This is the page object class for Google home page
+ **/
+
 public class GooglePage {
 
     public void search(String text) {
---
+++ src/test/BaseTest.java
@@ -0,0 +1,43 @@
+import com.maxsoft.testngtestresultsanalyzer.TestAnalyzeReportListener;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
+import static com.maxsoft.testngtestresultsanalyzer.DriverHolder.getDriver;
+import static com.maxsoft.testngtestresultsanalyzer.DriverHolder.setDriver;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Project Name    : selenide-page-object-demo
+ * Developer       : Osanda Deshan
+ * Version         : 1.0.0
+ * Date            : 9/9/2021
+ * Time            : 8:47 AM
+ * Description     : This is the Base Test class for all the test classes
+ **/
+
+@Listeners(TestAnalyzeReportListener.class)
+public class BaseTest {
+
+    @BeforeMethod
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        setDriver(driver);
+        setWebDriver(getDriver());
+        getDriver().manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(10, SECONDS);
+        open("https://duckduckgo.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        closeWebDriver();
+    }
+}
